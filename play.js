9677b84a47c5ae647efd47aa274d55cdd4df72e4
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-05-05 14:22:43 -0600

    Moved setupInput function to new file input.js

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 // establishes a connection with the game server
 console.log("Connecting ...");
 connect();
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-
-  const handleUserInput = (key) => {
-    if (key === '\u0003') {
-      process.exit();
-     }
-  };
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-
-
 setupInput();
