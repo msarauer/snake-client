2a31234705c8b535b60bcf41ea945be127a8ad09
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-05-05 14:35:23 -0600

    Added movement

--- input.js
+++ input.js
@@ -1,17 +1,33 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  const handleUserInput = (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    if (key === 'w') {
+      conn.write("Move: up");
+    }
+    if (key === 'a') {
+      conn.write("Move: left");;
+    }
+    if (key === 's') {
+      conn.write("Move: down");;
+    }
+    if (key === 'd') {
+      conn.write("Move: right");;
+    }
+  };
   stdin.on("data", handleUserInput);
   return stdin;
 };
 
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-   }
-};
+
 
 module.exports = {
   setupInput
