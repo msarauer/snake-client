7b3dae6d91f47e94e4d82eff3e8d524fc8c107f9
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-05-05 14:18:04 -0600

    Added ctrl-c to exit

--- play.js
+++ play.js
@@ -3,3 +3,23 @@ const {connect} = require('./client');
 // establishes a connection with the game server
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+
+  const handleUserInput = (key) => {
+    if (key === '\u0003') {
+      process.exit();
+     }
+  };
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+
+setupInput();
